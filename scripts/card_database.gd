650f55dbcfcf43f3777720e303248b7e25dbdb3d
thirdBlackCrow <thethreeblackcrows@gmail.com>
2025-03-07 14:15:30 +0100

    Card Flip animation

---
+++ scripts/card_database.gd
@@ -0,0 +1,7 @@
+const CARDS = {# Attack,Health
+	
+	"Knight":[2,3],
+	"Archer":[1,1],
+	"Demon":[5,7],
+	
+}
--- scripts/deck.gd
+++ scripts/deck.gd
@@ -3,16 +3,18 @@ class_name Deck
 const CARD_SCENE_PATH = "res://scenes/card.tscn" 
 const CARD_DRAW_SPEED = 0.2
 
-var player_deck = ["Knight","Knight","Knight"]
+var player_deck = ["Knight","Archer","Demon", "Knight"]
+var card_database_reference
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	player_deck.shuffle()
 	$RichTextLabel.text = str(player_deck.size())
-
+	card_database_reference = preload("res://scripts/card_database.gd")
 
 func draw_card():
-	var card_drawn = player_deck[0]
-	player_deck.erase(card_drawn)
+	var card_drawn_name = player_deck[0]
+	player_deck.erase(card_drawn_name)
 	
 	if player_deck.size() == 0:
 		$Area2D/CollisionShape2D.disabled = true
@@ -22,6 +24,11 @@ func draw_card():
 	$RichTextLabel.text = str(player_deck.size())
 	var card_scene = preload(CARD_SCENE_PATH)
 	var new_card = card_scene.instantiate()
+	var card_image_path = str("res://assets/"+card_drawn_name+"Card.png")
+	new_card.get_node("CardImage").texture = load(card_image_path)
+	new_card.get_node("Attack").text = str(card_database_reference.CARDS[card_drawn_name][0])
+	new_card.get_node("Health").text = str(card_database_reference.CARDS[card_drawn_name][1])
 	$"../CardManager".add_child(new_card)
 	new_card.name = "Card"
 	$"../PlayerHand".add_card_to_hand(new_card,CARD_DRAW_SPEED)
+	new_card.get_node("AnimationPlayer").play("card_flip")
